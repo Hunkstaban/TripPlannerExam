1e68b1a2887101e67fbc9428f01b44ffa505f65f
petert <peter.torgersen@gmail.com>
2024-11-08 07:42:01 +0100

    Made sure an ADMIN also has access to USER endpoints

--- src/main/java/dat/routes/TripRoutes.java
+++ src/main/java/dat/routes/TripRoutes.java
@@ -14,15 +14,15 @@ public class TripRoutes {
     public EndpointGroup getRoutes() {
         return () -> {
             get("/", tripController::getAllTrips, Role.ANYONE); // Get all trips
-            get("/{id}", tripController::getTripById, Role.USER); // Get a trip by ID
-            post("/", tripController::createTrip, Role.USER); // Create a new trip
+            get("/{id}", tripController::getTripById, Role.USER, Role.ADMIN); // Get a trip by ID
+            post("/", tripController::createTrip, Role.USER, Role.ADMIN); // Create a new trip
             put("/{id}", tripController::updateTrip, Role.ADMIN); // Update a trip by ID
             delete("/{id}", tripController::deleteTrip, Role.ADMIN); // Delete a trip by ID
             put("/{tripId}/guides/{guideId}", tripController::addGuideToTrip, Role.ADMIN); // Add an existing guide to an existing trip
             post("/populate", tripController::populateDatabase, Role.ANYONE); // Populate the database with trips and guides
-            get("/category/{category}", tripController::filterTripsByCategory, Role.USER); // Filter trips by category
-            get("/guides/overview", guideController::getGuidePriceOverview, Role.USER); // Guide trip overview
-            get("/{id}/packweight", tripController::getTotalPackingWeight, Role.USER); // Total packing weight endpoint
+            get("/category/{category}", tripController::filterTripsByCategory, Role.USER, Role.ADMIN); // Filter trips by category
+            get("/guides/overview", guideController::getGuidePriceOverview, Role.USER, Role.ADMIN); // Guide trip overview
+            get("/{id}/packweight", tripController::getTotalPackingWeight, Role.USER, Role.ADMIN); // Total packing weight endpoint
         };
     }
 
